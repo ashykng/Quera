32d0cd1f7b1aed0b94af6b40a06d03c96da09220
Ashkan Tavassoli <ashkant939@gmail.com>
2024-07-29 21:05:14 +0330

    problemset 72874

---
+++ https-quera.org-problemset-72874/Solution.java
@@ -0,0 +1,10 @@
+public class Solution
+{
+	public static void main(String[] args) {
+		app();
+	}
+
+	private static void app(){
+		System.out.print("Bebakhshid, vali shoma nemitoonin too in mosabeghe sherkat konin :(");
+	}
+}
---
+++ https-quera.org-problemset-72874/Solution.py
@@ -0,0 +1,5 @@
+def app():
+    print("Bebakhshid, vali shoma nemitoonin too in mosabeghe sherkat konin :(")
+
+if __name__ == "__main__":
+    app()
