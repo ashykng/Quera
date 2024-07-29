c8292b94c78ae2935a74889fc0329aae24ac319a
Ashkan Tavassoli <ashkant939@gmail.com>
2024-07-29 21:14:52 +0330

    problemset 123798

---
+++ https-quera.org-problemset-123798/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        app();
+    }
+
+    public static void app() {
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        System.out.println("Hello CodeCup " + userInput + "!");
+    }
+}
---
+++ https-quera.org-problemset-123798/Solution.py
@@ -0,0 +1,6 @@
+def app():
+    user_input = int(input())
+    print(f"Hello CodeCup {user_input}!")
+
+if __name__ == "__main__":
+    app()
